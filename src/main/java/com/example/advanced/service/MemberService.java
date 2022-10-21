d0cf2e4ee94f2bb5cf636393f3ab15a23cf5546e
DESKTOP-PE8J3UP\주은 <4708408@naver.com>
2022-10-21 13:35:13 +0900

    Docs: First Commit

---
+++ src/main/java/com/example/advanced/service/MemberService.java
@@ -0,0 +1,134 @@
+package com.example.advanced.service;
+
+import com.example.advanced.controller.request.LoginRequestDto;
+import com.example.advanced.controller.request.MemberRequestDto;
+import com.example.advanced.controller.request.TokenDto;
+import com.example.advanced.controller.response.MemberResponseDto;
+import com.example.advanced.controller.response.ResponseDto;
+import com.example.advanced.domain.Member;
+import com.example.advanced.domain.RefreshToken;
+import com.example.advanced.jwt.TokenProvider;
+import com.example.advanced.repository.MemberRepository;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+  private final MemberRepository memberRepository;
+
+  private final PasswordEncoder passwordEncoder;
+  private final AuthenticationManagerBuilder authenticationManagerBuilder;
+  private final TokenProvider tokenProvider;
+
+  @Transactional
+  public ResponseDto<?> createMember(MemberRequestDto requestDto) {
+    if (null != isPresentMember(requestDto.getNickname())) {
+      return ResponseDto.fail("DUPLICATED_NICKNAME",
+          "nickname is duplicated");
+    }
+
+    if (!requestDto.getPassword().equals(requestDto.getPasswordConfirm())) {
+      return ResponseDto.fail("PASSWORDS_NOT_MATCHED",
+          "password and password confirm are not matched");
+    }
+
+    Member member = Member.builder()
+            .nickname(requestDto.getNickname())
+                .password(passwordEncoder.encode(requestDto.getPassword()))
+                    .build();
+    memberRepository.save(member);
+    return ResponseDto.success(
+        MemberResponseDto.builder()
+            .id(member.getId())
+            .nickname(member.getNickname())
+            .createdAt(member.getCreatedAt())
+            .modifiedAt(member.getModifiedAt())
+            .build()
+    );
+  }
+
+  @Transactional
+  public ResponseDto<?> login(LoginRequestDto requestDto, HttpServletResponse response) {
+    Member member = isPresentMember(requestDto.getNickname());
+    if (null == member) {
+      return ResponseDto.fail("MEMBER_NOT_FOUND",
+          "member not found");
+    }
+
+    UsernamePasswordAuthenticationToken authenticationToken =
+        new UsernamePasswordAuthenticationToken(requestDto.getNickname(), requestDto.getPassword());
+    Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+    TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+    tokenToHeaders(tokenDto, response);
+
+    return ResponseDto.success(
+        MemberResponseDto.builder()
+            .id(member.getId())
+            .nickname(member.getNickname())
+            .createdAt(member.getCreatedAt())
+            .modifiedAt(member.getModifiedAt())
+            .build()
+    );
+  }
+
+  @Transactional
+  public ResponseDto<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+    if (!tokenProvider.validateToken(request.getHeader("Refresh-Token"))) {
+      return ResponseDto.fail("INVALID_TOKEN", "refresh token is invalid");
+    }
+    Member member = tokenProvider.getMemberFromAuthentication();
+    if (null == member) {
+      return ResponseDto.fail("MEMBER_NOT_FOUND",
+          "member not found");
+    }
+
+    Authentication authentication = tokenProvider.getAuthentication(request.getHeader("Access-Token"));
+    RefreshToken refreshToken = tokenProvider.isPresentRefreshToken(member);
+
+    if (!refreshToken.getValue().equals(request.getHeader("Refresh-Token"))) {
+      return ResponseDto.fail("INVALID_TOKEN", "refresh token is invalid");
+    }
+
+    TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+    refreshToken.updateValue(tokenDto.getRefreshToken());
+    tokenToHeaders(tokenDto, response);
+    return ResponseDto.success("success");
+  }
+
+  public ResponseDto<?> logout(HttpServletRequest request) {
+    if (!tokenProvider.validateToken(request.getHeader("Refresh-Token"))) {
+      return ResponseDto.fail("INVALID_TOKEN", "refresh token is invalid");
+    }
+    Member member = tokenProvider.getMemberFromAuthentication();
+    if (null == member) {
+      return ResponseDto.fail("MEMBER_NOT_FOUND",
+          "member not found");
+    }
+
+    return tokenProvider.deleteRefreshToken(member);
+  }
+
+  @Transactional(readOnly = true)
+  public Member isPresentMember(String nickname) {
+    Optional<Member> optionalMember = memberRepository.findByNickname(nickname);
+    return optionalMember.orElse(null);
+  }
+
+  public void tokenToHeaders(TokenDto tokenDto, HttpServletResponse response) {
+    response.addHeader("Access-Token", "Bearer " + tokenDto.getAccessToken());
+    response.addHeader("Refresh-Token", "Bearer " + tokenDto.getRefreshToken());
+    response.addHeader("Access-Token-Expire-Time", tokenDto.getAccessTokenExpiresIn().toString());
+  }
+
+}
